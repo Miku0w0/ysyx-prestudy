47a6ebffe99052daaa986ed312f47fb5d2b4ab1d
20238131022-Li Jinyuan <20238131022@m.scnu.edu.cn>
2025-11-16 11:54:46 +0800

    复习lab3代码并且修改了makefile

--- lab3/vsrc/top.v
+++ lab3/vsrc/top.v
@@ -1,63 +1,74 @@
-module top(a, b, ctrl, res, car, of);
-  input [3:0] a,b;
-  input [2:0] ctrl;
-  output reg [3:0] res;
-  output reg car, of;
-  reg [3:0] tmp;
-  always_latch@(a or b or ctrl) begin
+module top(
+    input [3:0] a,
+    input [3:0] b,
+    input [2:0] ctrl,
+    output reg [3:0] res,
+    output reg zero,
+    output reg carry, 
+    output reg overflow
+  );
+
+  reg [3:0] temp;
+
+always@(*) begin
+    zero = 0;
+    temp = 0;
     case(ctrl)
       3'b000:
         begin
-          {car, res}=a+b;
-          of=(a[3]==b[3])&&(res[3]!=a[3]);
+          {carry, res} = a + b;
+          overflow = (a[3] == b[3])&&(res[3] != a[3]);
         end
       3'b001:
         begin
-          tmp=((4'b1111)^b)+1;
-          {car, res}=a+tmp;
-          of=(a[3]==b[3])&&(res[3]!=a[3]);
+          temp = ((4'b1111) ^ b) + 1;
+          {carry, res} = a + temp;
+          overflow = (a[3] == b[3])&&(res[3] != a[3]);
         end
       3'b010:
         begin
-          res=~a;
-          car=0;
-          of=0;
+          res = ~a;
+          carry = 0;
+          overflow = 0;
         end
       3'b011:
         begin
-          res=a&b;
-          car=0;
-          of=0;
+          res = a & b;
+          carry = 0;
+          overflow = 0;
         end
       3'b100:
         begin
-          res=a|b;
-          car=0;
-          of=0;
+          res = a | b;
+          carry = 0;
+          overflow = 0;
         end
       3'b101:
         begin
-          res=a^b;
-          car=0;
-          of=0;
+          res = a ^ b;
+          carry = 0;
+          overflow = 0;
         end
       3'b110:
         begin
-          car=0;
-          of=0;
-          if(a[3]==0&&b[3]==1)
-            res=0;
-          else if(a[3]==1&&b[3]==0)
-            res=1;
+          carry = 0;
+          overflow = 0;
+          if(a[3] == 1&&b[3] == 0)
+            res = 1;
+          else if(a[3] == 0&&b[3] == 1)
+            res = 0;
           else
-            res=(a<b)?1:0;
+            res=(a < b) ? 1 : 0;
         end
       3'b111:
         begin
-          car=0;
-          of=0;
-          res=(a==b)?1:0;
+          carry = 0;
+          overflow = 0;
+          res = (a == b) ? 1 : 0;
         end
     endcase
+
+    zero = (res == 4'b0000) ? 1 : 0;
+
   end
 endmodule
