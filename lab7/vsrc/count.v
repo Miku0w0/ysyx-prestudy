2e4fd153c7f8bd21169ba4fc234b7887f73d6640
20238131022-Li Jinyuan <20238131022@m.scnu.edu.cn>
2025-11-18 00:54:38 +0800

    计数的清零逻辑更新

--- lab7/vsrc/count.v
+++ lab7/vsrc/count.v
@@ -8,20 +8,19 @@ module count(
     always @(posedge clk or negedge rst_n) begin
         if (rst_n) begin
             key_count <= 8'h00;   // BCD 00
-        end else if (key_press_edge) begin
-
-            // 个位未满 9，直接 +1
-            if (key_count[3:0] < 4'd9) begin
+        end 
+        else if (key_press_edge) begin
+            // 处理个位
+            if (key_count[3:0] < 4'd9) begin  
                 key_count[3:0] <= key_count[3:0] + 4'd1;
-            
-            // 个位满 9，则个位清零，处理十位
-            end else begin
-                key_count[3:0] <= 4'd0;
-
+            end 
+            // 处理十位
+            else begin  
+                key_count[3:0] <= 4'd0;  // 清零
                 if (key_count[7:4] < 4'd9)
-                    key_count[7:4] <= key_count[7:4] + 4'd1;
+                    key_count[7:4] <= key_count[7:4] + 4'd1;  
                 else
-                    key_count[7:4] <= 4'd0; // 十位满 9，整个清零
+                    key_count[7:4] <= 4'd0;  // 清零
             end
         end
     end
