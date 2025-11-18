afd8c09efe130cafb278d07c39f0f85b796c4f1f
20238131022-Li Jinyuan <20238131022@m.scnu.edu.cn>
2025-11-18 13:18:28 +0800

    理清了lab6的思路，更改初始值更加随机

--- lab6/vsrc/debounce.v
+++ lab6/vsrc/debounce.v
@@ -10,18 +10,23 @@ module debounce (
 
     always @(posedge clk or negedge reset_n) begin
         if (!reset_n) begin
-            btn_shift_reg <= 4'b1111;
+            btn_shift_reg <= 4'b0000;
             btn_state <= 1'b0;
-        end else begin
+        end 
+        else begin
+            // 这是移位寄存器的赋值
             btn_shift_reg <= {btn_shift_reg[2:0], button};
-            if (btn_shift_reg == 4'b0000 && !btn_state) begin
+            //  确保按键被按下，设置按键的状态
+            if (btn_shift_reg == 4'b1111 && !btn_state) begin
                 btn_state <= 1'b1;
-            end else if (btn_shift_reg == 4'b1111) begin
+            end 
+            //  确保按键已松开，设置按键的状态
+            else if (btn_shift_reg == 4'b0000) begin
                 btn_state <= 1'b0;
             end
         end
     end
-
+    // 上升沿检测
     always @(posedge clk or negedge reset_n) begin
         if (!reset_n)
             btn_state_prev <= 1'b0;
--- lab6/vsrc/lfsr.v
+++ lab6/vsrc/lfsr.v
@@ -8,17 +8,18 @@ module lfsr (
     wire feedback;
     reg [7:0] next_lfsr;
 
+    //  f(x) = x^8 + x^6 + x^5 + x^4 + 1(x^0)
     assign feedback = lfsr_reg[7] ^ lfsr_reg[5] ^ lfsr_reg[4] ^ lfsr_reg[3];
 
     always @(*) begin
         next_lfsr = {feedback, lfsr_reg[7:1]};
         if (next_lfsr == 8'b00000000)
-            next_lfsr = 8'b10011100;  // 避免全零状态
+            next_lfsr = 8'b10101101;  // 避免全零状态
     end
 
     always @(posedge clk or negedge reset_n) begin
         if (!reset_n)
-            lfsr_reg <= 8'b10011100;  // 初始化值
+            lfsr_reg <= 8'b10101101;  // 初始化值
         else if (btn_pulse)
             lfsr_reg <= next_lfsr;
     end
