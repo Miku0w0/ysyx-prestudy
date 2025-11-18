53e354f29604d513e15b03d68ba59857620d7c63
20238131022-Li Jinyuan <20238131022@m.scnu.edu.cn>
2025-11-18 12:39:51 +0800

    lab7接收器逻辑理清

--- lab7/vsrc/ps2_receiver.v
+++ lab7/vsrc/ps2_receiver.v
@@ -25,9 +25,10 @@ module ps2_receiver(
     // 检测PS/2时钟下降沿(数据在此时更新)
     wire ps2_sampling = ps2_clk_sync[2] & ~ps2_clk_sync[1];
 
-    // PS/2数据接收逻辑
+    // PS/2数据接收逻辑 : 校验 + F0 break 处理 + 边沿输出
     always @(posedge clk or negedge rst_n) begin
         if (rst_n) begin
+            // SW0清零时刻
             ps2_buffer <= 10'd0;
             ps2_count <= 4'd0;
             key_code <= 8'd0;
@@ -35,35 +36,39 @@ module ps2_receiver(
             prev_code <= 8'd0;
             break_flag <= 1'b0;
             key_press_edge <= 1'b0;
-        end else begin
-            key_press_edge <= 1'b0;  // 默认无边沿
-            
+        end 
+        else begin
+            // 默认无按下边沿，未清零时刻
+            key_press_edge <= 1'b0;  
             if (ps2_sampling) begin
-                if (ps2_count == 4'd10) begin  // 一帧数据接收完成(10位数据)
+                // 若有采样为下降边沿
+                if (ps2_count == 4'd10) begin  
+                    // 一帧数据接收完成(10位数据)，count清零
                     ps2_count <= 4'd0;
-
-                    // 验证起始位、停止位和奇偶校验
-                    if ((ps2_buffer[0] == 1'b0) && (ps2_data == 1'b1) && (^ps2_buffer[9:1])) begin
+                    if ((ps2_buffer[0] == 1'b0) && (ps2_data == 1'b1) && (^ps2_buffer[9:0])) begin
+                        // 验证起始位、停止位和奇偶校验
                         prev_code <= ps2_buffer[8:1];
-                        
-                        // 检测断码(0xF0)，表示按键松开
                         if (ps2_buffer[8:1] == 8'hF0) begin
+                            // 检测断码(0xF0)，表示按键松开
                             break_flag <= 1'b1;
                             key_press_reg <= 1'b0;  // 按键松开
-                        end else begin
+                        end 
+                        else begin
                             // 如果收到断码后的扫描码，表示按键松开
                             if (break_flag) begin
                                 break_flag <= 1'b0;
                                 key_press_reg <= 1'b0;  // 按键松开
-                            end else begin
-                                // 正常按键按下 - 产生一个边沿信号
+                            end 
+                            else begin
+                                // 正常按键按下，会产生一个边沿信号
                                 key_code <= ps2_buffer[8:1];
                                 key_press_reg <= 1'b1;  // 按键按下
                                 key_press_edge <= 1'b1; // 产生按下边沿
                             end
                         end
                     end
-                end else begin
+                end 
+                else begin
                     // 逐位接收数据
                     ps2_buffer[ps2_count] <= ps2_data;
                     ps2_count <= ps2_count + 4'd1;
